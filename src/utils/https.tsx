a8bfc057ebd32915278f90b7a1b5c67c2387e98f
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 18:46:20 +0200

    fix: change return type in fetchArticles in https file

--- src/utils/https.tsx
+++ src/utils/https.tsx
@@ -4,5 +4,5 @@ const BASE_URL = "https://newsapi.org/v2";
 const topHeadLinesApi = "/top-headlines";
 const API_KEY = "9023532f11354ed38c4c22521bc965ef";
 export const fetchArticles = async () => {
-   await axios.get(BASE_URL + topHeadLinesApi, { params: { country: "us", category: "business", apiKey: API_KEY } })
+   return await axios.get(BASE_URL + topHeadLinesApi, { params: { country: "us", category: "business", apiKey: API_KEY } })
 }
