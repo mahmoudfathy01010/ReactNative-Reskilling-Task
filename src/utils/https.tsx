7ddbc83612b082c1e889f3b2ea1584d6cdade41c
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 17:33:30 +0200

    feat: add fetchArticles Api to get Articles

--- src/utils/https.tsx
+++ src/utils/https.tsx
@@ -1,1 +1,8 @@
-export const BASE_URL = "https://newsapi.org/v2"
+import axios, { Axios } from "axios"
+
+const BASE_URL = "https://newsapi.org/v2";
+const topHeadLinesApi = "/top-headlines";
+const API_KEY = "9023532f11354ed38c4c22521bc965ef";
+export const fetchArticles = () => {
+    axios.get(BASE_URL + topHeadLinesApi, { params: { country: "us", category: "business", apiKey: API_KEY } })
+}
