05fa4d2b1c109538cbdc8707ea0c0d6aef3c2331
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 19:48:01 +0200

    feat: dispatch setError in thunkGetArticles

--- src/store/redux/news-actions.tsx
+++ src/store/redux/news-actions.tsx
@@ -2,7 +2,7 @@ import { AnyAction } from 'redux'
 import { RootState } from './store'
 import { ThunkAction } from 'redux-thunk'
 import { fetchArticles, validateStatus } from '../../utils/https'
-import { getNewsArticles } from './news-slice'
+import { getNewsArticles, setError } from './news-slice'
 import { Article } from '../model/article'
 
 export const thunkGetArticles =
@@ -18,12 +18,12 @@ export const thunkGetArticles =
                     return articles;
                 }
             }
-            try{
-            let articles : Article[]= await getData();
-            dispatch(getNewsArticles(articles));
+            try {
+                let articles: Article[] = await getData();
+                dispatch(getNewsArticles(articles));
             }
-            catch(err){
-                
+            catch (err) {
+                setError('Sorry, Could not fetch articles data!');
             }
-            
+
         }
