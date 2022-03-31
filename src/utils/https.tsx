a3e240b2d069be94dda846312ab4cff65f2b9c57
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 17:39:01 +0200

    feat: make fetch articles asyncronous

--- src/utils/https.tsx
+++ src/utils/https.tsx
@@ -3,6 +3,6 @@ import axios, { Axios } from "axios"
 const BASE_URL = "https://newsapi.org/v2";
 const topHeadLinesApi = "/top-headlines";
 const API_KEY = "9023532f11354ed38c4c22521bc965ef";
-export const fetchArticles = () => {
+export const fetchArticles = async () => {
     axios.get(BASE_URL + topHeadLinesApi, { params: { country: "us", category: "business", apiKey: API_KEY } })
 }
