a4feb91a5cc7df6fa19f0e7e451651ec6b70b102
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 19:13:07 +0200

    feat: wrap HomeScreen with redux Provider

--- App.tsx
+++ App.tsx
@@ -12,15 +12,17 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
-  View,
 } from 'react-native';
+import { Provider } from 'react-redux';
 import { HomeScreen } from './src/screens/HomeScreen';
+import { store } from './src/store/redux/store';
 
 const App = () => {
   return (
     <SafeAreaView>
+      <Provider store={store}>
       <HomeScreen></HomeScreen>
+      </Provider>
     </SafeAreaView>
   );
 };
