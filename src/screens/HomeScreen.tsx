404619cb1427824af5afdf8b4a0fc1eabf8191f9
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 19:14:14 +0200

    feat: getData in home using useEffect

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import {View, Text} from 'react-native'
-export const HomeScreen: React.FC = ()=>{
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native'
+import { useAppDispatch } from '../hooks';
+import { thunkGetArticles } from '../store/redux/news-actions';
+export const HomeScreen: React.FC = () => {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(thunkGetArticles);
+    }, [dispatch])
     return <View>
         <Text>
             Hello World
