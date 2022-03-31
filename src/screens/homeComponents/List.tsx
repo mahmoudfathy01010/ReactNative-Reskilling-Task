63feed37604140b066c102fbdf3d450ffcd07bb8
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 20:36:35 +0200

    feat: create homeList component

---
+++ src/screens/homeComponents/List.tsx
@@ -0,0 +1,16 @@
+import { FlatList, Text, View } from "react-native"
+import React from "react"
+import { Article } from "../../store/model/article"
+interface Props {
+    articles: Article[]
+}
+export const HomeList: React.FC<Props> = ({articles}) => {
+
+    return <View>
+        <FlatList data={articles}
+            renderItem={({ item, index }) => (<Text>
+                {item.title}
+            </Text>)}
+            keyExtractor={item => item.title} ></FlatList>
+    </View>
+}
