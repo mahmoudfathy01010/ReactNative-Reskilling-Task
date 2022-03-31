758cc9e66737b7bbc992d57298996c602c083ead
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 21:18:48 +0200

    feat: add image to article item

--- src/screens/homeComponents/articleItem/ArticleItem.tsx
+++ src/screens/homeComponents/articleItem/ArticleItem.tsx
@@ -1,4 +1,4 @@
-import {Text, View } from "react-native"
+import {Image, Text, View } from "react-native"
 import React from "react"
 import { Article } from "../../../store/model/article"
 import { styles } from "./style"
@@ -6,8 +6,9 @@ interface Props {
     article: Article
 }
 export const ArticleItem: React.FC<Props> = ({article}) => {
-
+    console.log(article)
     return <View style={styles.mainContainer}>
         <Text>{article.title}</Text>
+        <Image source={{uri: article.urlToImage}} style={styles.image}/>
     </View>
 }
