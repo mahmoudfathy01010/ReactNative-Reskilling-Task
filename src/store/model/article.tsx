01f006e9f3b3121b2d305e4c083e384f07571f6f
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 21:10:04 +0200

    feat: complete article model as api

--- src/store/model/article.tsx
+++ src/store/model/article.tsx
@@ -1,7 +1,27 @@
-export class Article{
-    title:string;
+export class Article {
+    title: string;
+    urlToImage: string;
+    publishedAt: string;
+    description: string;
+    content: string;
+    source: Source;
 
-    constructor(title: string){
+    constructor(title: string, urlToImage: string, publishedAt: string, description: string, content: string, source: Source) {
         this.title = title;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.description = description;
+        this.content = content;
+        this.source = source;
+    }
+}
+
+class Source {
+    id: string;
+    name: string;
+
+    constructor(id: string, name: string) {
+        this.id = id;
+        this.name = name
     }
 }
