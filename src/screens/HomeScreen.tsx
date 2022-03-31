a1b9593c91d6a3369d05ddc776a30180fc3fad59
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 20:57:01 +0200

    feat: add style file to HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,13 +3,14 @@ import { View, Text } from 'react-native'
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { thunkGetArticles } from '../store/redux/news-actions';
 import { HomeList } from './homeComponents/List';
+import { styles } from './style';
 export const HomeScreen: React.FC = () => {
     const dispatch = useAppDispatch();
     const articles = useAppSelector((state) => state.sliceReducer.list);
     useEffect(() => {
         dispatch(thunkGetArticles());
     }, [dispatch])
-    return <View>
+    return <View style ={styles.mainContainer} >
         {articles.length != 0 && <HomeList articles={articles}></HomeList>}
     </View>
 }
