3ac09bb1ef627aba25063b49c68efa1d22081de9
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 01:41:43 +0200

    feat: wrap article item with pressible

--- src/screens/homeScreen/articleItem/ArticleItem.tsx
+++ src/screens/homeScreen/articleItem/ArticleItem.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from "react-native"
+import { Image, Pressable, Text, View } from "react-native"
 import React from "react"
 import { Article } from "../../../store/model/article"
 import { styles } from "./style"
@@ -7,13 +7,18 @@ interface Props {
     article: Article
 }
 export const ArticleItem: React.FC<Props> = ({ article }) => {
-    return <View style={styles.mainContainer}>
-        <View style ={styles.imageContainer}>
-            <ImagWithPlaceHolder imageUrl= {article.urlToImage} title={article.title}></ImagWithPlaceHolder>
+    const onItemPress =()=>{
+
+    }
+    return <Pressable onPress={onItemPress}>
+        <View style={styles.mainContainer}>
+            <View style={styles.imageContainer}>
+                <ImagWithPlaceHolder imageUrl={article.urlToImage} title={article.title}></ImagWithPlaceHolder>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.titleText} numberOfLines={1}>{article.title}</Text>
+                <Text style={styles.descriptionText} numberOfLines={4}>{article.description}</Text>
+            </View>
         </View>
-        <View style={styles.infoContainer}>
-            <Text style={styles.titleText}numberOfLines={1}>{article.title}</Text>
-            <Text style={styles.descriptionText} numberOfLines={4}>{article.description}</Text>
-        </View>
-    </View>
+    </Pressable>
 }
