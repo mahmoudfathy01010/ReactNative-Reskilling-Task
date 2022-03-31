82f8101a5cc03bc80891b397bca397a3c6c6797c
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 01:15:19 +0200

    feat: add navigation stack to app file

--- App.tsx
+++ App.tsx
@@ -6,17 +6,20 @@ import {
 import { Provider } from 'react-redux';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { store } from './src/store/redux/store';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
       <Provider store={store}>
         <NavigationContainer>
-      <HomeScreen></HomeScreen>
-      </NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name='homeScreen' component={HomeScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
-    </SafeAreaView>
   );
 };
 
