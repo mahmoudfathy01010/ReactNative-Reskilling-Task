99e46ab8403f37213b332652ee930caf36dde61c
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 19:42:31 +0200

    feat: add error attribute to news-slice state

--- src/store/redux/news-slice.tsx
+++ src/store/redux/news-slice.tsx
@@ -2,9 +2,10 @@ import { createSlice } from "@reduxjs/toolkit";
 import { Article } from "../model/article";
 import { RootState } from "./store";
 interface NewsState {
-    list: Article[]
+    list: Article[],
+    errorMsg: string
 }
-const initialNewsState: NewsState = { list: [] }
+const initialNewsState: NewsState = { list: [], errorMsg: "" }
 const newsSlice = createSlice({
     name: 'news',
     initialState: initialNewsState,
@@ -16,5 +17,6 @@ const newsSlice = createSlice({
 })
 
 export const { getNewsArticles } = newsSlice.actions;
-export const articles = (state: RootState) => state.sliceReducer.list
+export const articles = (state: RootState) => state.sliceReducer.list;
+export const error = (state: RootState) => state.sliceReducer.errorMsg;
 export const sliceReducer = newsSlice.reducer;
