db0c7183b75f8830f24d4657950ef0bb368006d6
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 15:57:01 +0200

    feat: add news slice to redux state

---
+++ src/store/redux/news-slice.tsx
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialNewsState = {list: []}
+const newsSlice = createSlice({
+    name: 'news',
+    initialState: initialNewsState,
+    reducers:{
+        
+    }
+})
