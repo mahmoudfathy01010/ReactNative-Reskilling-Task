8304292399c60dc6c36ce34221d59e4dfc2ce668
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 18:36:59 +0200

    feat: add action param to getNewsArticles reducer in newsSlice

--- src/store/redux/news-slice.tsx
+++ src/store/redux/news-slice.tsx
@@ -1,20 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Article } from "../model/article";
 import { RootState } from "./store";
-interface NewsState{
-    list:Article[]
+interface NewsState {
+    list: Article[]
 }
-const initialNewsState : NewsState = {list: []}
+const initialNewsState: NewsState = { list: [] }
 const newsSlice = createSlice({
     name: 'news',
     initialState: initialNewsState,
-    reducers:{
-        getNewsArticles: (state) =>{
+    reducers: {
+        getNewsArticles: (state, action) => {
 
         }
     }
 })
 
-export const {getNewsArticles} = newsSlice.actions;
-export const articles = (state: RootState) => state.sliceReducer.list 
+export const { getNewsArticles } = newsSlice.actions;
+export const articles = (state: RootState) => state.sliceReducer.list
 export const sliceReducer = newsSlice.reducer;
