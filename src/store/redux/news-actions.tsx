70901737c2627a832cc883a4fb164965296e897d
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 18:45:30 +0200

    fix: fix  thunkGetArticles

--- src/store/redux/news-actions.tsx
+++ src/store/redux/news-actions.tsx
@@ -5,10 +5,12 @@ import { fetchArticles } from '../../utils/https'
 import { getNewsArticles } from './news-slice'
 
 export const thunkGetArticles =
-  (): ThunkAction<void, RootState, unknown, AnyAction> =>
-  async dispatch => {
-    const asyncResp = await fetchArticles()
-    dispatch(
-      getNewsArticles()
-    )
-  }
+    (): ThunkAction<void, RootState, unknown, AnyAction> =>
+        async dispatch => {
+            const getData = async () => {
+                const response = await fetchArticles();
+                dispatch(
+                    getNewsArticles(response)
+                )
+            }
+        }
