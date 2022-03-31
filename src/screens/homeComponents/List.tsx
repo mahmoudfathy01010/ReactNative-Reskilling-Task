ba8412de8d2a28d9f618a260802ce7c4e00fcce6
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 20:42:27 +0200

    feat: add ArticleItem component to HomeList component

--- src/screens/homeComponents/List.tsx
+++ src/screens/homeComponents/List.tsx
@@ -1,16 +1,16 @@
 import { FlatList, Text, View } from "react-native"
 import React from "react"
 import { Article } from "../../store/model/article"
+import { ArticleItem } from "./ArticleItem"
 interface Props {
     articles: Article[]
 }
-export const HomeList: React.FC<Props> = ({articles}) => {
+export const HomeList: React.FC<Props> = ({ articles }) => {
 
     return <View>
         <FlatList data={articles}
-            renderItem={({ item }) => (<Text>
-                {item.title}
-            </Text>)}
+            renderItem={({ item }) => (<ArticleItem article={item}>
+            </ArticleItem>)}
             keyExtractor={item => item.title} ></FlatList>
     </View>
 }
