eabd519de61917a806e85cf81ecf343cdfb39fc3
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-03-31 21:44:03 +0200

    feat: add style to article item

--- src/screens/homeComponents/articleItem/ArticleItem.tsx
+++ src/screens/homeComponents/articleItem/ArticleItem.tsx
@@ -1,14 +1,19 @@
-import {Image, Text, View } from "react-native"
+import { Image, Text, View } from "react-native"
 import React from "react"
 import { Article } from "../../../store/model/article"
 import { styles } from "./style"
 interface Props {
     article: Article
 }
-export const ArticleItem: React.FC<Props> = ({article}) => {
+export const ArticleItem: React.FC<Props> = ({ article }) => {
     console.log(article)
     return <View style={styles.mainContainer}>
-        <Text>{article.title}</Text>
-        <Image source={{uri: article.urlToImage}} style={styles.image}/>
+        <View style ={styles.imageContainer}>
+        <Image source={{ uri: article.urlToImage }} style={styles.image} />
+        </View>
+        <View style={styles.infoContainer}>
+            <Text style={styles.titleText}numberOfLines={2}>{article.title}</Text>
+            <Text style={styles.descriptionText} numberOfLines={3}>{article.description}</Text>
+        </View>
     </View>
 }
--- src/screens/homeComponents/articleItem/style.tsx
+++ src/screens/homeComponents/articleItem/style.tsx
@@ -2,6 +2,28 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     mainContainer: {
-        marginBottom: 10
+        marginBottom: 10,
+        flexDirection: 'row',
+        padding: 18
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 200,
+        marginEnd: 20,
+    },
+    imageContainer: {
+        flex: 1,
+        marginEnd: 10
+    },
+    infoContainer: {
+        flex: 2
+    },
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    descriptionText: {
+        fontSize: 14,
     }
 })
