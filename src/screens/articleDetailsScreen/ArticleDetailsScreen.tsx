be0af00d50f88bbd3d17851f2b911592d3b86eef
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 13:42:49 +0200

    feat: add info design

--- src/screens/articleDetailsScreen/ArticleDetailsScreen.tsx
+++ src/screens/articleDetailsScreen/ArticleDetailsScreen.tsx
@@ -11,6 +11,21 @@ export const ArticleDetailsScreen = () => {
         <View style={styles.imageContainer}>
             <ImagWithPlaceHolder imageUrl={article.urlToImage} style={styles.image}></ImagWithPlaceHolder>
         </View>
+        <View style={styles.infoContainer}>
+            <Text style={styles.titleText}>{article.title}</Text>
+            <View style={styles.metaContainer}>
+            <Text style={styles.autherText}>{"By: "+article.source.name}</Text>
+            <Text style={styles.autherText}>{"Date: "+article.publishedAt}</Text>
+            </View>
+            <View style={styles.descriptionContainer}>
+                <Text style={styles.headerStyle}>Descripion:</Text>
+                <Text style={styles.descriptionText}>{article.description}</Text>
+            </View>
+            <View style={styles.contentContainer}>
+                <Text style={styles.headerStyle}>Content:</Text>
+                <Text style={styles.contentText}>{article.content}</Text>
+            </View>
+        </View>
     </View>
 }
 
--- src/screens/articleDetailsScreen/style.tsx
+++ src/screens/articleDetailsScreen/style.tsx
@@ -14,6 +14,47 @@ export const styles = StyleSheet.create({
     },
     imageContainer: {
         flexDirection: 'row',
-        flex: 1,
     },
+    titleText: {
+        fontSize: 20,
+        fontWeight: '800',
+        color: colors.white
+    },
+    infoContainer:{
+        marginTop: 20,
+    },
+    descriptionText: {
+        marginTop: 6,
+        fontSize: 16,
+        fontWeight: '300',
+        color: colors.white
+    },
+    contentText: {
+        marginTop: 6,
+        fontSize: 16,
+        fontWeight: '300',
+        color: colors.white
+    },
+    headerStyle: {
+        marginTop: 6,
+        fontSize: 16,
+        fontWeight: '600',
+        color: colors.white
+    },
+    descriptionContainer: {
+        marginTop: 10,
+    },
+    contentContainer: {
+        marginTop: 10
+    },
+    autherText: {
+        marginTop: 6,
+        fontSize: 16,
+        fontWeight: '600',
+        color: colors.secondaryColor
+    },
+    metaContainer:{
+        flexDirection: 'row',
+        justifyContent:'space-between'
+    }
 })
