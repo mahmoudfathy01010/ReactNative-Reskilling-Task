cc1f6c115771b6ce027f1cb0d0e65eb946fd73ba
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 14:04:24 +0200

    feat: change path and query params

--- src/utils/https.tsx
+++ src/utils/https.tsx
@@ -1,10 +1,10 @@
 import axios, { Axios } from "axios"
 
 const BASE_URL = "https://newsapi.org/v2";
-const topHeadLinesApi = "/top-headlines";
+const topHeadLinesApi = "/everything";
 const API_KEY = "3e35d1236b194670a96df6d4b5adc033";
 export const fetchArticles = async () => {
-    return await axios.get(BASE_URL + topHeadLinesApi, { params: { country: "us", category: "business", apiKey: API_KEY } })
+    return await axios.get(BASE_URL + topHeadLinesApi, { params: {q:"tesla" ,apiKey: API_KEY } })
 }
 
 export const validateStatus = (status: number) => {
