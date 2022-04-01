86fb9f2b9fc8838e96c98068e13701c7fb2c001d
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 23:17:16 +0200

    feat: remove unused imports in app file

--- App.tsx
+++ App.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import {
   StyleSheet,
 } from 'react-native';
-import { HomeScreen, HOME_SCREEN_TAG } from './src/screens/homeScreen/HomeScreen';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { ArticleDetailsScreen, ARTICLE_DETAILS_SCREEN } from './src/screens/articleDetailsScreen/ArticleDetailsScreen';
-import { colors } from './src/utils/theme';
-import { useAppSelector } from './src/hooks';
 import { Tabs } from './src/screens/tabs/Tabs';
 
 export type RootStackParamList = {
