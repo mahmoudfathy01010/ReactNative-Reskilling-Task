30072a8aeda61706079030dfb315e7746f94e651
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 21:38:07 +0200

    feat: add settings design

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -1,8 +1,34 @@
-import { Text, View } from "react-native"
-import React from "react"
+import { Text, View, Switch } from "react-native"
+import React, { useState } from "react"
+import { useAppDispatch } from "../../hooks";
+import { setTheme } from "../../store/redux/theme.slice";
+import { useTheme } from "@react-navigation/native";
+import { styles } from "./style";
 
-export const SettingsScreen =()=>{
-    return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Settings!</Text>
-  </View>
+export const SettingsScreen = () => {
+    const{colors} = useTheme();
+    const dispatch = useAppDispatch(); 
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => {
+        if(isEnabled){
+            dispatch(setTheme("light"))
+        } 
+        else{
+            dispatch(setTheme("dark"))
+        }
+        setIsEnabled(previousState => !previousState);
+     }
+    return <View style={styles.mainContainer}>
+        <View style={styles.themeContainer}>
+        <Text style ={[styles.themeText, {color:colors.primary}]}>Set theme:</Text>
+
+        <Switch
+            trackColor={{ false: colors.text, true: colors.primary }}
+            thumbColor={isEnabled ? colors.notification : colors.background}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+        />
+        </View>
+    </View>
 }
---
+++ src/screens/settings/style.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    mainContainer:{
+        padding: 20,
+        flex:1,
+    },
+    themeContainer:{
+        flexDirection: 'row',
+        justifyContent:'space-between'
+    },
+    themeText:{
+        fontSize: 20
+    }
+}) 
