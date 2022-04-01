4d5c65313891253625bc9a28876df381ba7e338e
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 14:42:52 +0200

    feat: update Api key

--- src/utils/https.tsx
+++ src/utils/https.tsx
@@ -2,7 +2,7 @@ import axios, { Axios } from "axios"
 
 const BASE_URL = "https://newsapi.org/v2";
 const topHeadLinesApi = "/everything";
-const API_KEY = "3e35d1236b194670a96df6d4b5adc033";
+const API_KEY = "9023532f11354ed38c4c22521bc965ef";
 export const fetchArticles = async (query:string) => {
     console.log(query);
     return await axios.get(BASE_URL + topHeadLinesApi, { params: {q:query ,apiKey: API_KEY } })
