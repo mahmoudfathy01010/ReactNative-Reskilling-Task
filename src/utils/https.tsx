971400b470320451ad10c1126aadfc96b7928148
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 14:55:37 +0200

    feat: add default api call

--- src/utils/https.tsx
+++ src/utils/https.tsx
@@ -1,11 +1,18 @@
 import axios, { Axios } from "axios"
 
 const BASE_URL = "https://newsapi.org/v2";
-const topHeadLinesApi = "/everything";
-const API_KEY = "9023532f11354ed38c4c22521bc965ef";
+const topHeadLinesApi = "/top-headlines";
+const everythingApi = "/everything"
+const API_KEY = "9824c0c3006945f999909134fa45f473";
 export const fetchArticles = async (query:string) => {
-    console.log(query);
-    return await axios.get(BASE_URL + topHeadLinesApi, { params: {q:query ,apiKey: API_KEY } })
+    if(query.length>0){
+        console.log(query + "everything")
+        return await axios.get(BASE_URL + everythingApi, { params: {q:query ,apiKey: API_KEY } })
+    }
+    else{
+        console.log(query + "top")
+        return await axios.get(BASE_URL + topHeadLinesApi, { params: {country:"us" ,apiKey: API_KEY } })
+    }
 }
 
 export const validateStatus = (status: number) => {
