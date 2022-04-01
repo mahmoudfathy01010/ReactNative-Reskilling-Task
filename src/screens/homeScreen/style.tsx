4c9397959af02defdaaee7b3bc007e9504d12ce9
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-01 13:08:57 +0200

    improvement: remove text loading style

--- src/screens/homeScreen/style.tsx
+++ src/screens/homeScreen/style.tsx
@@ -25,9 +25,4 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    loadingText: {
-        color: colors.white,
-        fontSize: 20
-    },
-
 })
