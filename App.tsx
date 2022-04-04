f9228f62315a22583fc64a5d17dfa65527e926de
Mahmoud Fathy <mahmoudfathy01010@gmail.com>
2022-04-04 21:48:34 +0200

    feat: handle status bar theme

--- App.tsx
+++ App.tsx
@@ -1,53 +1,63 @@
 import { NavigatorScreenParams } from '@react-navigation/native';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {
+  StatusBar,
   StyleSheet,
+  View,
 } from 'react-native';
 import { Tabs } from './src/screens/tabs/Tabs';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useAppDispatch } from './src/hooks';
+import { useAppDispatch, useAppTheme } from './src/hooks';
 import { setTheme } from './src/store/redux/theme.slice';
-import { THEME_KEY } from './src/utils/theme';
+import { colors, DARK, LIGHT, THEME_KEY } from './src/utils/theme';
 import { LANG_KEY } from './src/utils/lang';
 import { setLang } from './src/store/redux/lang-slice';
 
 export type RootStackParamList = {
-  HomeScreenStack:NavigatorScreenParams<TabsParamList>
-  Settings:any
+  HomeScreenStack: NavigatorScreenParams<TabsParamList>
+  Settings: any
 };
 
 export type TabsParamList = {
   Home: undefined
-  MovieDetails: {id: string}
+  MovieDetails: { id: string }
 }
 
 const App = () => {
   const dispatch = useAppDispatch();
+  const appTheme = useAppTheme();
+  const getAppConfig = useCallback(async () => {
+    try {
+      const theme = await AsyncStorage.getItem(THEME_KEY);
+      if (theme != null) {
+        dispatch(setTheme(theme))
+      }
+      const lang = await AsyncStorage.getItem(LANG_KEY);
+      if (lang != null) {
+        dispatch(setLang(lang))
+      }
+      console.log(theme)
+      console.log(lang)
 
-   const getAppConfig = async()=> { 
-     try{
-       const theme = await AsyncStorage.getItem(THEME_KEY);
-       if(theme!=null){
-         dispatch(setTheme(theme))
-       }
-       const lang = await AsyncStorage.getItem(LANG_KEY);
-       if(lang!=null){
-         dispatch(setLang(lang))
-       }
-       console.log(theme)
-       console.log(lang)
 
-
-     }
-     catch(err){
-       console.log(err)
-     }
-   }
-   useEffect(()=>{
-     getAppConfig();
-   },[getAppConfig])
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }, [dispatch])
+  useEffect(() => {
+    getAppConfig();
+  }, [getAppConfig])
   return (
+    <View style={{ flex: 1 }}>
+      <StatusBar animated={true}
+        barStyle={appTheme.mode == DARK ? "light-content" : "dark-content"}
+        showHideTransition={'slide'}
+        backgroundColor={appTheme.theme.primary}
+        hidden={false}
+      />
       <Tabs></Tabs>
+    </View>
   );
 };
 
